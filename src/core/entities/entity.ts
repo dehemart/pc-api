a28b9de739d35a17027445b213e0222f725dac76
dehemart <martins.denis@gmail.com>
2022-10-18 10:42:53 -0300

    Add base entity

---
+++ src/core/entities/entity.ts
@@ -0,0 +1,22 @@
+import { v4 as uuidV4 } from 'uuid';
+
+export abstract class BaseEntity<T> {
+  private props: T;
+  private _id: string;
+  private _createAt: Date;
+
+  constructor( props:T, id?:string ){
+    this._id = id || uuidV4();
+    this.props = props;
+    this._createAt = new Date();
+  }
+
+  public get id(): string {
+    return this._id;
+  }
+
+  public get createAt(): Date {
+    return this._createAt;
+  }
+
+}
